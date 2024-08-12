18e54a8e280882c996ae0cf1ed63c4ef499b4849
Manideep <mbustudy@gmail.com>
2024-08-13 01:56:23 +0530

    Update index.js

--- classroom_backend/index.js
+++ classroom_backend/index.js
@@ -2,7 +2,11 @@ const connectToMango=require('./db')
 const express=require('express')
 const app=express();
 var cors=require('cors');
-app.use(cors());
+app.use(cors(
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET","DELETE"],
+    credentials:true
+));
 
 app.use(express.json());
 app.get('/',(req,res)=>{
